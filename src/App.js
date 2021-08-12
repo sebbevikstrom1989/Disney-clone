af7b0adc4858ebcb53c524d9d6b7d06483a2416a
sebbevikstrom1989 <sebastian@vikstroms.se>
2021-08-13 00:01:31 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Detail from "./components/Detail";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
